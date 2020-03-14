12d1c77833dc7464df9cf27400168da826e82ae9
Grigorii Horos <horosgrisa@gmail.com>
2020-03-14 20:15:17 +0200

    Optimize

--- colorize.plugin.zsh
+++ colorize.plugin.zsh
@@ -1,4 +1,15 @@
 #!/usr/bin/env zsh
+# Standarized $0 handling, following:
+# https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+if [[ $PMSPEC != *b* ]] {
+  PATH=$PATH:"${0:h}/bin"
+}
+
+DEPENDENCES_ARCH+=( grc )
+DEPENDENCES_DEBIAN+=( grc )
 
 export GREP_COLOR='4;31'
 export LESS_TERMCAP_mb=$'\E[00;32m'
@@ -9,129 +20,128 @@ export LESS_TERMCAP_se=$'\E[0m'
 export LESS_TERMCAP_us=$'\E[00;32m'
 export LESS_TERMCAP_ue=$'\E[0m'
 export LESS='-R -M'
-export YAOURT_COLORS="nb=1:pkg=1:ver=1;32:lver=1;45:installed=1;42:grp=1;34:od=1;41;5:votes=1;44:dsc=0:other=1;35"
 
-function grep(){
+function grep() {
   =grep --colour=auto "$@"
 }
 
-function egrep(){
+function egrep() {
   =egrep --colour=auto "$@"
 }
 
-function fgrep(){
+function fgrep() {
   =fgrep --colour=auto "$@"
 }
 
 if (( $+commands[grc] )); then
-  function env(){
+  function env() {
     =grc --colour=auto env "$@"
   }
 
-  function lsblk(){
+  function lsblk() {
     =grc --colour=auto lsblk "$@"
   }
 
-  function df(){
-    =grc --colour=auto df -h "$@" 
+  function df() {
+    =grc --colour=auto df -h "$@"
   }
 
-  function du(){
+  function du() {
     =grc --colour=auto du -h "$@"
   }
 
-  function free(){
+  function free() {
     =grc --colour=auto free -h "$@"
   }
 
-  function as(){
+  function as() {
     =grc --colour=auto as "$@"
   }
 
-  function diff(){
+  function diff() {
     =grc --colour=auto diff --color "$@"
   }
 
   if (( $+commands[dig] )); then
-    function dig(){
+    function dig() {
       =grc --colour=auto dig "$@"
     }
   fi
 
   if (( $+commands[gas] )); then
-    function gas(){
+    function gas() {
       =grc --colour=auto gas "$@"
     }
   fi
 
   if (( $+commands[gcc] )); then
-    function gcc(){
+    function gcc() {
       =grc --colour=auto gcc "$@"
     }
   fi
 
   if (( $+commands[g++] )); then
-    function g++(){
+    function g() ++(){
       =grc --colour=auto g++ "$@"
     }
   fi
 
   if (( $+commands[last] )); then
-    function last(){
+    function last() {
       =grc --colour=auto last "$@"
     }
   fi
 
   if (( $+commands[ld] )); then
-    function ld(){
+    function ld() {
       =grc --colour=auto ld "$@"
     }
   fi
 
   if (( $+commands[ifconfig] )); then
-    function ifconfig(){
+    function ifconfig() {
       =grc --colour=auto ifconfig "$@"
     }
   fi
 
   if (( $+commands[mount] )); then
-    function mount(){
+    function mount() {
       =grc --colour=auto mount "$@"
     }
   fi
 
   if (( $+commands[mtr] )); then
-    function mtr(){
+    function mtr() {
       =grc --colour=auto mtr "$@"
     }
   fi
 
   if (( $+commands[netstat] )); then
-    function netstat(){
+    function netstat() {
       =grc --colour=auto netstat "$@"
     }
   fi
 
   if (( $+commands[ping] )); then
-    function ping(){
+    function ping() {
       =grc --colour=auto ping "$@"
     }
   fi
 
   if (( $+commands[ping6] )); then
-    function ping6(){
+    function ping6() {
       =grc --colour=auto ping6 "$@"
     }
   fi
 
   if (( $+commands[ps] )); then
-    function ps(){
+    function ps() {
       =grc --colour=auto ps "$@"
     }
   fi
 
   if (( $+commands[traceroute] )); then
-    function traceroute(){
+    function traceroute() {
       =grc --colour=auto traceroute "$@"
     }
   fi
